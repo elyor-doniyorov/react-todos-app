d3d5a266782ebe4891471832da32a41f82ddcd54
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-09-20 17:43:15 +0500

    Add state and todos array of items to the parent element and fix linter errors

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 class TodoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Setup development environment',
+        completed: true,
+      },
+      {
+        id: 2,
+        title: 'Develop website and add content',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Deploy to live server',
+        completed: false,
+      },
+    ],
+  };
+
   render() {
     return (
-      <div>
-        <h1>Hello from Create React App</h1>
-        <p>We are in a React Component!</p>
-      </div>
-    )
+      <ul>
+        {this.state.todos.map(todo => (<li>{todo.title}</li>))};
+      </ul>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoContainer from './components/TodoContainer'
-
+import TodoContainer from './components/TodoContainer';
 
 ReactDOM.render(
   <React.StrictMode>
-    < TodoContainer />
-  </React.StrictMode>, 
- document.getElementById('root'));
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
