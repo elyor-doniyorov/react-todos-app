c3e23ee54f43a57627901346d336a52273a727b3
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-09-20 17:14:25 +0500

    Update index.js

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TodoContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from Create React App</h1>
+        <p>We are in a React Component!</p>
+      </div>
+    )
+  }
+}
+
+export default TodoContainer;
