00127ecf190ab7738425ab7633617ca380c60e56
elyor doniyorov <doniyorovelyor@gmail.com>
2021-09-21 15:41:25 +0500

    Using the setState updater

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -29,14 +29,17 @@ class TodoContainer extends React.Component {
   }
 
   handleChange = (id) => {
-    this.setState({
-      todos: this.state.todos.map((todo) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
         if (todo.id === id) {
-          todo.completed = !todo.completed;
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
         }
         return todo;
       }),
-    });
+    }));
   };
 
   render() {
