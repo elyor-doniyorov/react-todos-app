70d2c742817efe4469f1abf332d249c253ed8589
elyor doniyorov <doniyorovelyor@gmail.com>
2021-09-21 11:44:09 +0500

    Converting Class-Based Component to Function Component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,8 @@
 react/prop-types */
 import React from 'react';
 
-class TodoItem extends React.Component {
-  render() {
-    return <li>{this.props.todo.title}</li>;
-  }
+function TodoItem(props) {
+  return <li>{props.todo.title}</li>;
 }
 
 export default TodoItem;
