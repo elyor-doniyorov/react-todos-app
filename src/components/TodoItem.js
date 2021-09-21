2c2de955f59c24a8204f4bbe8745675847245d8d
elyor doniyorov <doniyorovelyor@gmail.com>
2021-09-21 15:23:14 +0500

    Add checkbox input and use handle events

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,16 @@ import React from 'react';
 
 class TodoItem extends React.Component {
   render() {
-    return <li>{this.props.todo.title}</li>;
+    return (
+      <li>
+        <input
+          type="checkbox"
+          checked={this.props.todo.completed}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        />
+        {this.props.todo.title}
+      </li>
+    );
   }
 }
 
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -8,7 +8,11 @@ class TodosList extends React.Component {
     return (
       <ul>
         {this.props.todos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} />
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleChangeProps={this.props.handleChangeProps}
+          />
         ))}
       </ul>
     );
