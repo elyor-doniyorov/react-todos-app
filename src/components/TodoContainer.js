5cf029ee2b6c0f4ac1bd58887c2e172c858eb43d
elyor doniyorov <doniyorovelyor@gmail.com>
2021-09-21 10:11:36 +0500

    Add TodoList.js and fix eslint errors

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,33 +1,41 @@
+/* eslint-disable react/prefer-stateless-function, react/state-in-constructor,
+react/destructuring-assignment */
 import React from 'react';
+import TodosList from './TodosList';
 
 class TodoContainer extends React.Component {
-  state = {
-    todos: [
-      {
-        id: 1,
-        title: 'Setup development environment',
-        completed: true,
-      },
-      {
-        id: 2,
-        title: 'Develop website and add content',
-        completed: false,
-      },
-      {
-        id: 3,
-        title: 'Deploy to live server',
-        completed: false,
-      },
-    ],
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ],
+    };
+  }
 
   render() {
     return (
-      <ul>
-        {this.state.todos.map(todo => (<li>{todo.title}</li>))};
-      </ul>
+      <div>
+        <TodosList todos={this.state.todos} />
+      </div>
     );
   }
 }
 
 export default TodoContainer;
+/* eslint-enable  react/prefer-stateless-function, react/state-in-constructor,
+react/destructuring-assignment */
---
+++ src/components/TodosList.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+
+class TodosList extends React.Component {
+  render() {
+    return (
+      <ul>
+        <li>first</li>
+        <li>second</li>
+        <li>third</li>
+      </ul>
+    );
+  }
+}
+
+export default TodosList;
+/* eslint-enable react/prefer-stateless-function */
