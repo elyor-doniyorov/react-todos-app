38ab1288d1ab20449cf5de6bae0626ac89817b7a
elyor doniyorov <doniyorovelyor@gmail.com>
2021-09-21 17:25:17 +0500

    Styling React App with CSS Modules

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,15 @@
 /* eslint-disable react/destructuring-assignment, react/prefer-stateless-function,
 react/prop-types, react/button-has-type */
 import React from 'react';
+import styles from './TodoItem.module.css';
 
 class TodoItem extends React.Component {
   render() {
     return (
-      <li>
+      <li className={styles.item}>
         <input
           type="checkbox"
+          className={styles.checkbox}
           checked={this.props.todo.completed}
           onChange={() => this.props.handleChangeProps(this.props.todo.id)}
         />
