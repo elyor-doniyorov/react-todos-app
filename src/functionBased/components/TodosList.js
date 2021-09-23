4894a470cfd0749d271d0c3750eb4998c7296846
elyor doniyorov <doniyorovelyor@gmail.com>
2021-09-23 15:52:24 +0500

    TodoList.js conversion

--- src/functionBased/components/TodosList.js
+++ src/functionBased/components/TodosList.js
@@ -3,24 +3,19 @@ react/prop-types, react/jsx-key */
 import React from 'react';
 import TodoItem from './TodoItem';
 
-class TodosList extends React.Component {
-  render() {
-    return (
-      <ul>
-        {this.props.todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            handleChangeProps={this.props.handleChangeProps}
-            deleteTodoProps={this.props.deleteTodoProps}
-            setUpdate={this.props.setUpdate}
-          />
-        ))}
-      </ul>
-    );
-  }
-}
-
+const TodosList = (props) => (
+  <ul>
+    {props.todos.map((todo) => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        handleChangeProps={props.handleChangeProps}
+        deleteTodoProps={props.deleteTodoProps}
+        setUpdate={props.setUpdate}
+      />
+    ))}
+  </ul>
+);
 export default TodosList;
 /* eslint-enable react/destructuring-assignment, react/prefer-stateless-function,
 react/prop-types, react/jsx-key */
