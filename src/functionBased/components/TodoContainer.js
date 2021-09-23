a0b65291d416ce4041b69573438003a2132e1a5c
elyor doniyorov <doniyorovelyor@gmail.com>
2021-09-23 16:57:51 +0500

    Router, Switch and The Link and the NavLink Component

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -1,11 +1,15 @@
 /* eslint-disable react/prefer-stateless-function, react/state-in-constructor,
 react/destructuring-assignment, react/no-access-state-in-setstate, no-param-reassign,
-react/sort-comp, no-use-before-define */
+react/sort-comp, no-use-before-define, import/extensions */
 import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { Route, Switch } from 'react-router-dom';
 import Header from './Header';
 import InputTodo from './InputTodo';
 import TodosList from './TodosList';
+import About from '../pages/About';
+import NotMatch from '../pages/NotMatch';
+import Navbar from './Navbar';
 
 const TodoContainer = () => {
   const [todos, setTodos] = useState(getInitialTodos());
@@ -74,22 +78,35 @@ const TodoContainer = () => {
   }, [todos]);
 
   return (
-    <div className="container">
-      <div className="inner">
-        <Header />
-        <InputTodo addTodoProps={addTodoItem} />
-        <TodosList
-          todos={todos}
-          handleChangeProps={handleChange}
-          deleteTodoProps={delTodo}
-          setUpdate={setUpdate}
-        />
-      </div>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <div className="container">
+            <div className="inner">
+              <Header />
+              <InputTodo addTodoProps={addTodoItem} />
+              <TodosList
+                todos={todos}
+                handleChangeProps={handleChange}
+                deleteTodoProps={delTodo}
+                setUpdate={setUpdate}
+              />
+            </div>
+          </div>
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="*">
+          <NotMatch />
+        </Route>
+      </Switch>
+    </>
   );
 };
 
 export default TodoContainer;
 /* eslint-enable  react/prefer-stateless-function, react/state-in-constructor,
 react/destructuring-assignment, react/no-access-state-in-setstate, no-param-reassign,
-react/sort-comp, no-use-before-define */
+react/sort-comp, no-use-before-define, import/extensions */
